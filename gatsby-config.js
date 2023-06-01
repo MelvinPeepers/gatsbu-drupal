422842bba6401f23bd96e6369ab17827a72654ab
Melvin Peepers <melvinpeepers@Melvins-MacBook-Pro.local>
2023-06-01 08:55:19 -0400

    setup Drupal and added settings to gatsby-config

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,16 +3,33 @@
  */
 module.exports = {
   siteMetadata: {
-    title: `gatsby-starter-drupal`,
-    siteUrl: `https://www.yourdomain.tld`,
+    title: `Gatsby CMS Drupal`,
   },
   plugins: [
     {
       resolve: `gatsby-source-drupal`,
       options: {
-        baseUrl: `https://live-contentacms.pantheonsite.io`,
-        apiBase: `api`
-      }
-    }
+        baseUrl: `https://live-gatsby-site-test.pantheonsite.io/`,
+        apiBase: `api`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: `UA-93349937-2`,
+      },
+    },
+    `gatsby-plugin-offline`,
+    `gatsby-plugin-glamor`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`,
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography.js`,
+      },
+    },
   ],
 }
+
+
